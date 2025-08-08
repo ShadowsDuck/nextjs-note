8201a4d873c37aafc4c4212c288ad7921863128d
Tanaphat Partoom <76895893+ShadowsDuck@users.noreply.github.com>
2025-08-08 08:26:46 +0700

    resend

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,43 +3,43 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db";
 import { schema } from "@/db/schema";
 import { nextCookies } from "better-auth/next-js";
-// import { Resend } from "resend";
-// import VerificationEmail from "@/components/emails/verification-email";
-// import PasswordResetEmail from "@/components/emails/reset-email";
+import { Resend } from "resend";
+import VerificationEmail from "@/components/emails/verification-email";
+import PasswordResetEmail from "@/components/emails/reset-email";
 
-// const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const auth = betterAuth({
-  // emailVerification: {
-  //   sendVerificationEmail: async ({ user, url }) => {
-  //     await resend.emails.send({
-  //       from: "onboarding@resend.dev",
-  //       to: [user.email],
-  //       subject: "Verify your email address",
-  //       react: VerificationEmail({
-  //         userName: user.name,
-  //         verificationUrl: url,
-  //       }),
-  //     });
-  //   },
-  //   sendOnSignUp: true,
-  // },
+  emailVerification: {
+    sendVerificationEmail: async ({ user, url }) => {
+      await resend.emails.send({
+        from: "no-reply@mail.nextjs-note-olive.vercel.app",
+        to: [user.email],
+        subject: "Verify your email address",
+        react: VerificationEmail({
+          userName: user.name,
+          verificationUrl: url,
+        }),
+      });
+    },
+    sendOnSignUp: true,
+  },
   emailAndPassword: {
     enabled: true,
     autoSignIn: false,
-    // requireEmailVerification: true,
-    // sendResetPassword: async ({ user, url }) => {
-    //   await resend.emails.send({
-    //     from: "NS Note <nsnote@tanaphatdev.com>",
-    //     to: [user.email],
-    //     subject: "Reset your password",
-    //     react: PasswordResetEmail({
-    //       userName: user.name,
-    //       resetUrl: url,
-    //       requestTime: new Date().toISOString(),
-    //     }),
-    //   });
-    // },
+    requireEmailVerification: true,
+    sendResetPassword: async ({ user, url }) => {
+      await resend.emails.send({
+        from: "no-reply@mail.nextjs-note-olive.vercel.app",
+        to: [user.email],
+        subject: "Reset your password",
+        react: PasswordResetEmail({
+          userName: user.name,
+          resetUrl: url,
+          requestTime: new Date().toISOString(),
+        }),
+      });
+    },
   },
   socialProviders: {
     google: {
